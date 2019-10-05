2db80413d2522db10fca1972e2ce25a050c47289
Adryan Hawkins <teamzaal@gmail.com>
2019-10-05 19:42:43 +0000

    Still no pictures

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
     belongs_to :user
     belongs_to :place
     mount_uploader :picture, PictureUploader
+    validates :picture, presence: true
+    validates :caption, presence: true, length: { minimum: 3 , maximum: 500}
 end
