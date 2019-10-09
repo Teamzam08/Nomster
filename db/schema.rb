c2d910a142bb7ff84f2d4b819d8b1a69c5496597
Adryan Hawkins <teamzaal@gmail.com>
2019-10-09 00:12:03 +0000

    Added User_id Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_005609) do
+ActiveRecord::Schema.define(version: 2019_10_08_235059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_005609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
     t.index ["place_id"], name: "index_photos_on_place_id"
   end
 
@@ -66,6 +67,9 @@ ActiveRecord::Schema.define(version: 2019_10_08_005609) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
+  end
+
+  create_table "user_i_dfor_photos", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
